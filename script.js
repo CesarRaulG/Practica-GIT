5d35bce28b54a0384fd4b0f7d04c04e57190d799
Cesar Raúl González Valentín <raulgv.ti20@utsjr.edu.mx>
2023-05-25 12:20:52 -0600

    Cuarto commit: Realizar cambios en estilos,script y index

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 console.log('¡Hola ');
 var nombre = 'Cesar';
 var edad = 20;
+
+var universidad = 'UTSJR'
+var materia = 'Desarollo '
 console.log('Mi nombre es ' + nombre + ' y tengo ' + edad + ' años.');
+console.log('Mi universidad es '+universidad + 'y el trabajo es de la materia '+ materia);
